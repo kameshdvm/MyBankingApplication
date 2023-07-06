2c455d6150adb59eaf9f7f84ddc18c8194e98dba
kameshdvm <kameshdvm@gmail.com>
2023-07-06 21:06:53 +0530

    added index page and home page and working now on transaction

---
+++ src/main/java/com/mybank/database/ReceiverRepository.java
@@ -0,0 +1,7 @@
+package com.mybank.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiverRepository extends JpaRepository<ReceiverBankDB,Integer> {
+
+}
